a9b0cf8f736686d16cbc0f7491e484ef48ea504c
Tenniel Miao <tennielmiao@gmail.com>
2018-05-18 11:02:10 -0500

    minor update - script001.py

--- script001.py
+++ script001.py
@@ -4,6 +4,15 @@ script001.py
 basic exploratory data analysis
 
 Author: Tianyi Miao
+
+TrackML CheatSheet
+hits:        ['hit_id', 'x', 'y', 'z', 'volume_id', 'layer_id', 'module_id']
+particles:   ['particle_id', 'vx', 'vy', 'vz', 'px', 'py', 'pz', 'q', 'nhits']
+cells:       ['hit_id', 'ch0', 'ch1', 'value']
+truth:       ['hit_id', 'particle_id', 'tx', 'ty', 'tz', 'tpx', 'tpy', 'tpz', 'weight']
+
+
+
 """
 import os
 
@@ -36,7 +45,9 @@ def get_event_name(event_id):
     return "event" + str(event_id).zfill(9)
 
 
-def add_features(hits_df):
+def add_features(hits_df, copy=True):
+    if copy:
+        hits_df = hits_df.copy()
     hits_df["r"] = np.sqrt(hits_df["x"]**2 + hits_df["y"]**2 + hits_df["z"]**2)
     hits_df["x*y"] = hits_df["x"] * hits_df["y"]
     hits_df["x*z"] = hits_df["x"] * hits_df["z"]
@@ -45,7 +56,17 @@ def add_features(hits_df):
     hits_df["x/r"] = hits_df["x"] / hits_df["r"]
     hits_df["y/r"] = hits_df["y"] / hits_df["r"]
     hits_df["z/r"] = hits_df["z"] / hits_df["r"]
+    return hits_df
 
+
+def normalize_features(hits_df, copy=True):
+    if copy:
+        hits_df = hits_df.copy()
+    hits_df["r"] = np.sqrt(hits_df["x"] ** 2 + hits_df["y"] ** 2 + hits_df["z"] ** 2)
+    hits_df["x"] /= hits_df["r"]
+    hits_df["y"] /= hits_df["r"]
+    hits_df["z"] /= hits_df["r"]
+    return hits_df
 
 print("Start executing script001.py")
 
@@ -55,32 +76,20 @@ test_events = np.random.choice(range(1000, 2820), size=1, replace=False)
 cols = ["x", "y", "z"]
 
 for event_id in test_events:
-    hits, cells, particles, truth = load_event(TRAIN_DIR + get_event_name(event_id),
-                                               [HITS, CELLS, PARTICLES, TRUTH])
-    # dbscan_1 = cluster.DBSCAN(eps=0.5, min_samples=1, metric='euclidean', algorithm='auto', leaf_size=30, p=None, n_jobs=1)
-    print("hits:\t\t", hits.columns.tolist())
-    print("particles:\t\t", particles.columns.tolist())
-    print("cells:\t\t", cells.columns.tolist())
-    print("truth:\t\t", truth.columns.tolist())
+    # hits, cells, particles, truth = load_event(TRAIN_DIR + get_event_name(event_id), [HITS, CELLS, PARTICLES, TRUTH])
+    hits, truth = load_event(TRAIN_DIR + get_event_name(event_id), [HITS, TRUTH])
 
-    for ind in np.random.choice(range(particles.shape[0]), size=20, replace=False):
-        temp_particle_id = particles.loc[ind, "particle_id"]
-        print("temporary particle id:", temp_particle_id)
-        print("number of hits:\t\t\t\t\t\t", particles.loc[ind, "nhits"], sep="")
-        print("number of corresponding hit_ids:", end="\t")
-        temp_hits = truth.loc[truth.particle_id == temp_particle_id, "hit_id"]
-        print(len(temp_hits))
-    """
+    dbscan_1 = cluster.DBSCAN(eps=0.1, min_samples=1, metric='euclidean', algorithm='auto', leaf_size=30, p=None, n_jobs=1)
+
+    normalize_features(hits, copy=False)
+    print("start predicting")
+
     pred = pd.DataFrame({
         "hit_id": hits.hit_id,
         "track_id": dbscan_1.fit_predict(hits[cols])
     })
-    """
-    if False:
-        # test scorer
-        shuffled = shuffle_hits(truth, 0.05)  # 5% probability to reassign a hit
-        print(shuffled)
-        print(score_event(truth=truth, submission=shuffled))
 
-
+    print(pred)
+    print("final score:")
+    print(score_event(truth=truth, submission=pred))
 
