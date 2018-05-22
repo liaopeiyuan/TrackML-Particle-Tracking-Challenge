32ce342ea14128501dbd1017e0ebb7b0bc29fa22
Alexander Liao <alexander_liao@outlook.com>
2018-05-22 17:42:47 +0200

    change test name to score_test; created requirements.txt

--- score_test.py
+++ score_test.py
@@ -1,6 +1,6 @@
 from trackml.dataset import load_event
 
-hits, cells, particles, truth = load_event('./portable-dataset/')
+hits, cells, particles, truth = load_event('./portable-dataset/train/event000001000')
 
 from trackml.randomize import shuffle_hits
 from trackml.score import score_event
