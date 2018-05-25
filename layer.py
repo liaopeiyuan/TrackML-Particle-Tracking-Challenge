7e50d01bbe80a49719f5f69471d448306b4293c4
Alexander Liao <alexander_liao@outlook.com>
2018-05-25 17:42:34 -0400

    updated layer.py

--- layer.py
+++ layer.py
@@ -9,23 +9,32 @@ from sklearn.preprocessing import normalize, scale
 numpy.random.seed(7)
 X=numpy.loadtxt('feature.csv',dtype='float',delimiter=',')
 Y=numpy.loadtxt('label.csv',dtype='float',delimiter=',')
-X=X[1:100000,:]
-Y=Y[1:100000,:]
-X=normalize(X,axis=0)
-X = scale( X, axis=0, with_mean=True, with_std=True, copy=True )
+Xtrain=X[1:200000,:]
+Ytrain=Y[1:200000,:]
+Xtrain=normalize(Xtrain,axis=0)
+Xtrain = scale( Xtrain, axis=0, with_mean=True, with_std=True, copy=True )
+Ytrain=normalize(Ytrain,axis=0)
+Ytrain = scale( Ytrain, axis=0, with_mean=True, with_std=True, copy=True )
+
+Xtest=X[200000:250000,:]
+Ytest=Y[200000:250000,:]
+Xtest=normalize(Xtest,axis=0)
+Xtest = scale( Xtest, axis=0, with_mean=True, with_std=True, copy=True )
+Ytest=normalize(Ytest,axis=0)
+Ytest = scale( Ytest, axis=0, with_mean=True, with_std=True, copy=True )
 print(type(X))
 print(type(Y))
 
 def nn_1(input_length):
     input_layer = Input(shape=(input_length,))
-    encoded = Dense(256, activation="tanh")(input_layer)
+    encoded = Dense(128, activation="tanh")(input_layer)
 
-    encoded = Dense(128, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
-    encoded = Dropout(0.5, noise_shape=None, seed=None)(encoded)
+    encoded = Dense(96, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
+    encoded = Dropout(0.1, noise_shape=None, seed=None)(encoded)
     encoded = Dense(96, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
     encoded = Dense(64, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
-    encoded = Dropout(0.5, noise_shape=None, seed=None)(encoded)
-    encoded = Dense(96, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
+    encoded = Dropout(0.1, noise_shape=None, seed=None)(encoded)
+    encoded = Dense(64, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
 
     #encoded = Dense(128, activation="tanh")(encoded)
     #encoded = Dense(96, activation="tanh")(encoded)
@@ -38,15 +47,15 @@ def nn_1(input_length):
     #encoded = Dense(96, activation="tanh")(encoded)
 
     decoded = Dense(64, activation="tanh",activity_regularizer=regularizers.l2(0.01))(encoded)
-    encoded = Dropout(0.5, noise_shape=None, seed=None)(encoded)
+    encoded = Dropout(0.1, noise_shape=None, seed=None)(encoded)
     decoded = Dense(96, activation="tanh",activity_regularizer=regularizers.l2(0.01))(decoded)
-    encoded = Dropout(0.5, noise_shape=None, seed=None)(encoded)
+    encoded = Dropout(0.1, noise_shape=None, seed=None)(encoded)
     decoded = Dense(128, activation="tanh",activity_regularizer=regularizers.l2(0.01))(decoded)
 
     decoded = Dense(3, activation="linear")(decoded)
     # encoder = Model(input_layer, encoded)
     nn_predictor = Model(input_layer, decoded)
-    nn_predictor.compile(optimizer="RMSprop", loss="mean_squared_error")  # mean_absolute_error ?
+    nn_predictor.compile(optimizer="adam", loss="mean_squared_logarithmic_error")  # mean_absolute_error ?
     return nn_predictor
 
 nn_predictor = nn_1(3)
