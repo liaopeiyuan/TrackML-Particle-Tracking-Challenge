39b46bb26e607f094e2a18c08f620d39addf3f1c
Tenniel Miao <tennielmiao@gmail.com>
2018-05-30 15:40:27 -0500

    fix critical bug in track/hit completeness

--- geometric/d02.py
+++ geometric/d02.py
@@ -22,7 +22,8 @@ def hit_completeness(df, idx, track_size):
     """
     num = (df.loc[idx, "particle_id"] != 0).sum()
     all_particles = df.loc[idx, "particle_id"].unique().tolist()
-    all_particles.remove(0)
+    if 0 in all_particles:
+        all_particles.remove(0)
     denom = track_size[all_particles].sum()
     return num / denom
 
@@ -32,9 +33,12 @@ def track_completeness(df, idx):
     (number of tracks with all hits in the region) / (number of tracks that have at least 1 hit in the region)
     """
     all_particles = df.loc[idx, "particle_id"].unique().tolist()
-    all_particles.remove(0)
+    if 0 in all_particles:
+        all_particles.remove(0)
 
-    agg_1 = df.loc[idx, :].groupby("particle_id", sort=True)["x"].agg("count").drop(0, inplace=False)
+    agg_1 = df.loc[idx, :].groupby("particle_id", sort=True)["x"].agg("count")
+    if 0 in agg_1:
+        agg_1.drop(0, inplace=True)
     agg_2 = df.loc[df.particle_id.isin(all_particles), :].groupby("particle_id", sort=True)["x"].agg("count")
     return np.mean(agg_1 == agg_2)
 
@@ -48,6 +52,7 @@ def subroutine_1(df):
 
     track_size = df.groupby("particle_id")["x"].agg("count")  # pd.Series; track id -> track size
 
+    # display track completeness and hit completeness
     step_az = np.pi / 20
     for az_0 in np.arange(0.0, np.pi, step_az):
         print("[{:.4f}, {:.4f}) ".format(az_0, az_0 + step_az), end="")
@@ -57,6 +62,39 @@ def subroutine_1(df):
         ))
 
 
+def subroutine_2(df):
+    # feature engineering
+    df.loc[:, "rc"] = np.sqrt(df.x ** 2 + df.y ** 2)  # radius in cylindrical coordinate
+    df.loc[:, "rs"] = np.sqrt(df.x ** 2 + df.y ** 2 + df.z ** 2)  # radius in spherical coordinate
+    df.loc[:, "ac"] = np.arctan2(df.y, df.x)  # from -pi to pi
+    df.loc[:, "az"] = np.arctan2(df.rc, df.z)  # from 0 to pi
+
+    for az_center in np.arange(0, 180, 1):
+        # az_center = 75
+        az_margin = 1
+        lo, hi = np.deg2rad(az_center - az_margin), np.deg2rad(az_center + az_margin)
+        idx = (df.az >= lo) & (df.az < hi)
+
+        print("[{:.4f}, {:.4f}] ".format(lo, hi),
+              str(idx.sum()).rjust(5),
+              " {:.6f}".format(hit_completeness(df, idx)))
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(df.loc[idx, "x"].values, df.loc[idx, "y"].values, df.loc[idx, "z"].values, ".")
+    plt.show()
+    """
+
+def subroutine_3(df):
+    # feature engineering
+    df.loc[:, "rc"] = np.sqrt(df.x ** 2 + df.y ** 2)  # radius in cylindrical coordinate
+    df.loc[:, "rs"] = np.sqrt(df.x ** 2 + df.y ** 2 + df.z ** 2)  # radius in spherical coordinate
+    df.loc[:, "ac"] = np.arctan2(df.y, df.x)  # from -pi to pi
+    df.loc[:, "az"] = np.arctan2(df.rc, df.z)  # from 0 to pi
+
+    # parameter for conic scanning
+
+
 if __name__ == "__main__":
     print("start running script d01.py")
 
@@ -64,4 +102,4 @@ if __name__ == "__main__":
     for hits, truth in s1.remove_train_events(n=10, content=[s1.HITS, s1.TRUTH], randomness=True)[1]:
         print("=" * 120)
         hits = hits.merge(truth, how="left", on="hit_id")
-        subroutine_1(hits)
+        subroutine_2(hits)
