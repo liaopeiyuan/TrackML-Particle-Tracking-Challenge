89b163e765190c63b928f3368907eb3223640202
Alexander Liao <alexander_liao@outlook.com>
2018-06-03 22:31:23 -0400

    upload helix_error function in utils

--- geometric/utils.py
+++ geometric/utils.py
@@ -90,3 +90,78 @@ def track_completeness(df, idx):
         agg_1.drop(0, inplace=True)
     agg_2 = df.loc[df.particle_id.isin(all_particles), :].groupby("particle_id", sort=True)["x"].agg("count")
     return np.mean(agg_1 == agg_2)
+
+
+
+
+def helix_error(x,y,z,x0,y0,damp,iter,left,verbose=False):
+    
+    a=2
+    b=6
+
+    for i in range(iter):
+        x_est = a * np.cos(b * z)+x0
+        y_est = a * np.sin(b * z)+y0
+        
+        errx = (x.flatten() - x_est)
+        erry = (y.flatten() - y_est)
+        
+        if np.mean(errx)<1e-5 and np.mean(erry)<1e-5:
+            break
+
+        Jxa = np.cos(b * z)
+
+        #print(np.dot(z,np.sin(b*z)))
+        Jxb = -1*a* z* np.sin( b * z)
+        
+        #print(Jxa.shape)
+        #print(Jxb.shape)
+        
+        Jx = np.vstack((Jxa,Jxb))
+        #print(Jx)
+        #print(np.dot(np.linalg.inv(np.dot(Jx,Jx.T)),Jx).T)
+        gradx = np.dot( np.dot(np.linalg.inv(np.dot(Jx,Jx.T)),Jx),errx)
+        
+        a = a + damp*gradx[0]
+        b = b + damp*gradx[1]
+        
+        if verbose:
+            print('a update wrt x on iter {}: {}'.format(i,a))
+            print('b update wrt x on iter {}: {}'.format(i,b))
+
+        Jya = np.sin(b * z)
+        Jyb = a*z*np.cos(b*z)
+        Jy = np.vstack((Jya,Jyb))
+        grady = np.dot( np.dot(np.linalg.inv(np.dot(Jy,Jy.T)),Jy),erry)
+        
+        a = a + damp*grady[0]
+        b = b + damp*grady[1]
+
+        if verbose:
+            print('a update wrt y on iter {}: {}'.format(i,a))
+            print('b update wrt y on iter {}: {}'.format(i,b))
+
+        """
+        J=np.zeros((2,2))
+        J[0,0]=np.sin(b*z)
+        J[0,1]=
+        J[1,0]=
+        J[1,1]=
+        """
+
+    x_est = a * np.cos(b * z)+x0
+    y_est = a * np.sin(b * z)+y0
+        
+    errx = (x.flatten() - x_est)
+    erry = (y.flatten() - y_est)
+    
+    return a,b,errx,erry
+
+x=np.array([1.46,-0.62,-2.21,-2.07,-0.31,1.7])
+y=np.array([1.9,2.31,0.92,-1.20,-2.38,-1.69])
+z=np.array([1,2,3,4,5,6])
+[a,b,errx,erry]=helix_error(x,y,z,0,0,0.00005,500,1,verbose=True)
+print(a)
+print(b)
+print(errx)
+print(erry)
