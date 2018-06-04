1812871c263667090fce592412be87c6b4d7a578
Alexander Liao <alexander_liao@outlook.com>
2018-06-04 19:04:26 -0400

    updated nn

--- neural_network/init.py
+++ neural_network/init.py
@@ -56,9 +56,27 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 import sklearn.preprocessing
+from sklearn.cluster import dbscan
+from sklearn.preprocessing import scale
+
+from trackml.score import score_event
+
+# KAIL libs
 
 
-#---------------------------------------------------------------------------------
+from utils.session import Session
+from geometric.helix import HelixUnroll
+from geometric.tools import merge_naive, reassign_noise, label_encode, hit_completeness
+
+
+
+from geometric.tools import merge_naive, merge_discreet
+from geometric.helix import HelixUnroll
+from utils.session import Session
+
+
+
+
 
 # https://stackoverflow.com/questions/34968722/how-to-implement-the-softmax-function-in-python
 def np_softmax(x):
@@ -85,31 +103,31 @@ def save_pickle_file(pickle_file, x):
 print('@%s:  ' % os.path.basename(__file__))
 
 
-if 1:
-    SEED = 35202 #1510302253  #int(time.time()) #
-    random.seed(SEED)
-    np.random.seed(SEED)
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed_all(SEED)
-    print ('\tset random seed')
-    print ('\t\tSEED=%d'%SEED)
 
-if 1:
-    torch.backends.cudnn.benchmark = True  ##uses the inbuilt cudnn auto-tuner to find the fastest convolution algorithms. -
-    torch.backends.cudnn.enabled   = True
-    print ('\tset cuda environment')
-    print ('\t\ttorch.__version__              =', torch.__version__)
-    print ('\t\ttorch.version.cuda             =', torch.version.cuda)
-    print ('\t\ttorch.backends.cudnn.version() =', torch.backends.cudnn.version())
-    try:
-        print ('\t\tos[\'CUDA_VISIBLE_DEVICES\']     =',os.environ['CUDA_VISIBLE_DEVICES'])
-        NUM_CUDA_DEVICES = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
-    except Exception:
-        print ('\t\tos[\'CUDA_VISIBLE_DEVICES\']     =','None')
-        NUM_CUDA_DEVICES = 1
+SEED = int(time.time()) 
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+print ('\tset random seed')
+print ('\t\tSEED=%d'%SEED)
 
-    print ('\t\ttorch.cuda.device_count()      =', torch.cuda.device_count())
-    print ('\t\ttorch.cuda.current_device()    =', torch.cuda.current_device())
+
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.enabled   = True
+print ('\tset cuda environment')
+print ('\t\ttorch.__version__              =', torch.__version__)
+print ('\t\ttorch.version.cuda             =', torch.version.cuda)
+print ('\t\ttorch.backends.cudnn.version() =', torch.backends.cudnn.version())
+try:
+    print ('\t\tos[\'CUDA_VISIBLE_DEVICES\']     =',os.environ['CUDA_VISIBLE_DEVICES'])
+    NUM_CUDA_DEVICES = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+except Exception:
+    print ('\t\tos[\'CUDA_VISIBLE_DEVICES\']     =','None')
+    NUM_CUDA_DEVICES = 1
+
+print ('\t\ttorch.cuda.device_count()      =', torch.cuda.device_count())
+print ('\t\ttorch.cuda.current_device()    =', torch.cuda.current_device())
 
 
 print('')
