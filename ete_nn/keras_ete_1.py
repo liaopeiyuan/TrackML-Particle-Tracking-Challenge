827e5823ba6c3adbdeda23fc5e0602a8de317f84
Tianyi Miao <tennielmiao@gmail.com>
2018-07-16 13:13:58 -0500

    update end-to-end NN training procedure

--- ete_nn/keras_ete_1.py
+++ ete_nn/keras_ete_1.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 
 import keras
+from keras.layers import Input, Dense
+from keras.models import Model
+
+
+from utils.session import Session
 
 
 def get_quadratic_features(df):
@@ -46,3 +51,36 @@ def join_hits_truth(hits, truth):
     hits.drop("hit_id", axis=1, inplace=True)
     return hits
 
+
+def get_basic_nn():
+    nn_dict = {"main_input": Input(shape=(9,), name="main_input")}
+    nn_dict["x1"] = Dense(64, activation="relu", name="x1")(nn_dict["main_input"])
+    nn_dict["x2"] = Dense(128, activation="relu", name="x2")(nn_dict["x1"])
+    nn_dict["x3"] = Dense(256, activation="relu", name="x3")(nn_dict["x2"])
+    nn_dict["x4"] = Dense(256, activation="relu", name="x4")(nn_dict["x3"])
+    nn_dict["x5"] = Dense(256, activation="relu", name="x5")(nn_dict["x4"])
+    nn_dict["x6"] = Dense(128, activation="relu", name="x6")(nn_dict["x5"])
+    nn_dict["x7"] = Dense(64, activation="relu", name="x7")(nn_dict["x6"])
+    return nn_dict
+
+
+if __name__ == "__main__":
+    print("start running basic neural network")
+    np.random.seed(1)  # restart random number generator
+    s1 = Session(parent_dir="E:/TrackMLData/")
+    n_events = 4
+    count = 0
+    nn_dict_basic = get_basic_nn()
+    for hits, truth in s1.get_train_events(n=n_events, content=[s1.HITS, s1.TRUTH], randomness=True)[1]:
+        count += 1
+        print(f"{count}/{n_events}")
+        hits = join_hits_truth(hits, truth)
+        fx = get_feature(hits, 0.0, flip=False, quadratic=True)
+        fy = get_target(hits)
+        print(f"shape of fx: {fx.shape}")
+        print(f"shape of fy: {fy.shape}")
+        n_targets = fy.shape[1]
+        output_layer = Dense(n_targets, activation="softmax", name="temp_output")(nn_dict_basic["x7"])
+        temp_model = Model(inputs=nn_dict_basic["main_input"], outputs=output_layer)
+        temp_model.compile(optimizer="adam", loss="categorical_crossentropy")
+        temp_model.fit(fx, fy, batch_size=32, verbose=1, validation_split=0.1)
