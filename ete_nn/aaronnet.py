f220ae777e550e80c03604c30b27fc7fbaf09486
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-07-29 09:24:30 -0700

    Fixing indentation inconsistency

--- ete_nn/aaronnet.py
+++ ete_nn/aaronnet.py
@@ -75,12 +75,12 @@ def train_nn(nn_list, train_x, train_y, basic_trainable=True, epochs=10, batch_s
     output_layer = Dense(n_targets, activation="softmax", trainable=True)(nn_list[-1])
     if os.listdir("./checkpoint/aaronmao/") != []:
         print("Model present, loading model")
-        temp_model.load_model("./checkpoint/aaronmao/mymodel.h5")
+        temp_model = load_model("./checkpoint/aaronmao/mymodel.h5")
         temp_model = keras.utils.multi_gpu_model(temp_model, gpus=8)
     else:
         print("Model not present, creating model")
         temp_model = Model(inputs=nn_list[0], outputs=output_layer)
-        temp_model.save(".checkpoint/aaronmao/mymodel.h5")
+	temp_model.save("./checkpoint/aaronmao/mymodel.h5")
         temp_model = keras.utils.multi_gpu_model(temp_model, gpus=8)
 
     adam = keras.optimizers.adam(lr=0.001)
@@ -111,7 +111,7 @@ def main():
         for i in range(100):
             print("Step: " + str(i))
             loss, model = train_nn(nn_list_basic, get_feature(hits_train, theta=np.random.rand() * 2 * np.pi, flip=np.random.rand() < 0.5, quadratic=True), permute_target(fy),
-            basic_trainable=True, epochs=2, batch_size=2048, verbose=1)
+            basic_trainable=True, epochs=50, batch_size=2048, verbose=1)
 
             if(loss<loss_global):
                 print("Epoch result better than the best, saving model")              
