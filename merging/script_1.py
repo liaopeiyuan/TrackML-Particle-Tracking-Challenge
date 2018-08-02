88d83a2f17240d7fe0d81b973101da5ff62eaae5
Tenniel Miao <tennielmiao@gmail.com>
2018-08-01 23:06:46 -0400

    fix indentation issues

--- merging/script_1.py
+++ merging/script_1.py
@@ -4,8 +4,18 @@ test the clustering score from alex
 
 import numpy as np
 import pandas as pd
+
+from sklearn.cluster import DBSCAN
 from geometric.tools import merge_naive
 from utils.session import Session
+
+
+class HelixUnroll3(object):
+    """
+    (Tianyi) this is my 3rd version of HelixUnroll, which seeks to emulate that by Alex Liao
+    """
+
+
 
 if __name__ == '__main__':
     np.random.seed(0)
--- utils/session.py
+++ utils/session.py
@@ -37,7 +37,7 @@ class Session(object):
     CELLS = "cells"
     PARTICLES = "particles"
     TRUTH = "truth"
-
+    
     def __init__(self, parent_dir="./", train_dir="train/", test_dir="test/", detectors_dir="detectors.csv",
                  sample_submission_dir="sample_submission.csv"):
         """
@@ -50,10 +50,10 @@ class Session(object):
         self._test_dir = test_dir
         self._detectors_dir = detectors_dir
         self._sample_submission_dir = sample_submission_dir
-
+        
         if not os.path.isdir(self._parent_dir):
             raise ValueError("The input parent directory {} is invalid.".format(self._parent_dir))
-
+        
         # there are 8850 events in the training dataset; some ids from 1000 to 9999 are skipped
         if os.path.isdir(self._parent_dir + self._train_dir):
             self._train_event_id_list = sorted(
@@ -61,24 +61,24 @@ class Session(object):
         else:
             self._train_dir = None
             self._train_event_id_list = []
-
+        
         if os.path.isdir(self._parent_dir + self._test_dir):
             self._test_event_id_list = sorted(
                 set(int(x[x.index("0"):x.index("-")]) for x in os.listdir(self._parent_dir + self._test_dir)))
         else:
             self._test_dir = None
             self._test_event_id_list = []
-
+        
         if not os.path.exists(self._parent_dir + self._detectors_dir):
             self._detectors_dir = None
-
+        
         if not os.path.exists(self._parent_dir + self._sample_submission_dir):
             self._sample_submission_dir = None
-
+    
     @staticmethod
     def get_event_name(event_id):
         return "event" + str(event_id).zfill(9)
-
+    
     def get_train_events(self, n=10, content=(HITS, TRUTH), randomness=True):
         n = min(n, len(self._train_event_id_list))
         if randomness:
@@ -86,11 +86,11 @@ class Session(object):
         else:
             event_ids, = self._train_event_id_list[:n]
             self._train_event_id_list = self._train_event_id_list[n:] + self._train_event_id_list[:n]
-
+        
         event_names = [Session.get_event_name(event_id) for event_id in event_ids]
         return event_names, \
-            (load_event(self._parent_dir + self._train_dir + event_name, content) for event_name in event_names)
-
+               (load_event(self._parent_dir + self._train_dir + event_name, content) for event_name in event_names)
+    
     def remove_train_events(self, n=10, content=(HITS, TRUTH), randomness=True):
         """
         get n events from self._train_event_id_list:
@@ -107,11 +107,11 @@ class Session(object):
                 self._train_event_id_list.remove(event_id)
         else:
             event_ids, self._train_event_id_list = self._train_event_id_list[:n], self._train_event_id_list[n:]
-
+        
         event_names = [Session.get_event_name(event_id) for event_id in event_ids]
         return event_names, \
-            (load_event(self._parent_dir + self._train_dir + event_name, content) for event_name in event_names)
-
+               (load_event(self._parent_dir + self._train_dir + event_name, content) for event_name in event_names)
+    
     def get_test_event(self, n=3, content=(HITS, TRUTH), randomness=True):
         n = min(n, len(self._test_event_id_list))
         if randomness:
@@ -119,10 +119,10 @@ class Session(object):
         else:
             event_ids, = self._test_event_id_list[:n]
             self._test_event_id_list = self._test_event_id_list[n:] + self._test_event_id_list[:n]
-
+        
         event_names = [Session.get_event_name(event_id) for event_id in event_ids]
         return event_names, \
-            (load_event(self._parent_dir + self._test_dir + event_name, content) for event_name in event_names)
+               (load_event(self._parent_dir + self._test_dir + event_name, content) for event_name in event_names)
     
     def remove_test_events(self, n=10, content=(HITS, CELLS), randomness=False):
         n = min(n, len(self._test_event_id_list))
@@ -134,8 +134,8 @@ class Session(object):
             event_ids, self._test_event_id_list = self._test_event_id_list[:n], self._test_event_id_list[n:]
         event_names = [Session.get_event_name(event_id) for event_id in event_ids]
         return event_names, \
-            (load_event(self._parent_dir + self._test_dir + event_name, content) for event_name in event_names)
-
+               (load_event(self._parent_dir + self._test_dir + event_name, content) for event_name in event_names)
+    
     def make_submission(self, predictor, path):
         """
         :param predictor: function, predictor(hits: pd.DataFrame, cells: pd.DataFrame)->np.array
@@ -145,7 +145,7 @@ class Session(object):
         sub_list = []  # list of predictions by event
         for event_id in self._test_event_id_list:
             event_name = Session.get_event_name(event_id)
-
+            
             hits, cells = load_event(self._parent_dir + self._test_dir + event_name, (Session.HITS, Session.CELLS))
             pred = predictor(hits, cells)  # predicted cluster labels
             sub = pd.DataFrame({"hit_id": hits.hit_id, "track_id": pred})
